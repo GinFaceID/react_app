ea3375f61471b5cca649222c2c993e1483012fe7
Chepond <chepond@cheponds-mbp.mynet>
2020-02-25 00:03:03 +0800

    Updates

---
+++ precache-manifest.06a63f8a3475d03555e84c0106280f91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27be91f19c2177bef8d0eb750857d2c5",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "c7a93c926411bff7eb87",
+    "url": "/react_app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "57012281975265d2db9f",
+    "url": "/react_app/static/css/main.29b8b983.chunk.css"
+  },
+  {
+    "revision": "c7a93c926411bff7eb87",
+    "url": "/react_app/static/js/2.40a567c4.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/react_app/static/js/2.40a567c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57012281975265d2db9f",
+    "url": "/react_app/static/js/main.6e14888d.chunk.js"
+  },
+  {
+    "revision": "e46ca46ba4d555d9a740",
+    "url": "/react_app/static/js/runtime-main.6a706dfb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d6394da0b36807b0637c8642fccaab6d",
+    "url": "/react_app/static/media/logo.d6394da0.png"
+  }
+]);
