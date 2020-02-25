272352eca6ac4e2d09584080fa3506cb9a5d43b5
Chepond <chepond@cheponds-mbp.mynet>
2020-02-25 22:43:16 +0800

    123

---
+++ build/precache-manifest.6d428e2679a6905e5306a6e06f4d0a5d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a228e1b3ee6de20457f2262bd96ccd",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "24b34444c4d9ea9e7141",
+    "url": "/react_app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2389f148dfa04559c8a9",
+    "url": "/react_app/static/css/main.29b8b983.chunk.css"
+  },
+  {
+    "revision": "24b34444c4d9ea9e7141",
+    "url": "/react_app/static/js/2.d8985792.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/react_app/static/js/2.d8985792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2389f148dfa04559c8a9",
+    "url": "/react_app/static/js/main.360bbeb8.chunk.js"
+  },
+  {
+    "revision": "e46ca46ba4d555d9a740",
+    "url": "/react_app/static/js/runtime-main.6a706dfb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d6394da0b36807b0637c8642fccaab6d",
+    "url": "/react_app/static/media/logo.d6394da0.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import AppRouter from './appRouter/appRouter';
 import { Container, Row, Col } from 'react-bootstrap';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
+import {WhatsappShareButton, WhatsappIcon} from 'react-share';
 
 function App() {
   return <Container>
@@ -17,6 +18,20 @@ function App() {
       appId="638776666894882"
       themeColor="#EAD7C3"
     />
+    <WhatsappShareButton
+            url={"bit.ly/2mcMygY"}
+            title={""}
+            separator=":: "
+            className="Demo__some-network__share-button"
+            style={{position: 'fixed',
+              right:20,
+              bottom: 12,
+              borderRadius: 35,
+              boxShadow: "2px 3px 6px  #9E9E9E",
+            backgroundColor:'#25D366'}}
+          >
+            <WhatsappIcon size={70} round />
+          </WhatsappShareButton>
   </Container>
 }
 
