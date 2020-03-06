9998d09e74dde5a4ec0e3e73585539c4a7b100eb
Chepond <chepond@Cheponds-MacBook-Pro.local>
2020-03-06 11:11:26 +0800

    new

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,74 +3,37 @@ import axios from 'axios';
 import { Card } from 'react-bootstrap';
 import './main.css';
 
-const instagramTitleRegExp = new RegExp(/<title>\n*(.*)\(@gin_faceid.hk\) • Instagram photos and videos\n*<\/title>/)
-const instagramRegExp = new RegExp(/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/);
 export default class Main extends React.Component {
 
     state = {
         photos: [],
         title: ""
     }
-    query = `{
-        story(slug:"abc"){
-          title,
-          igPost{
-            displayUrl,
-            caption
-          }
-        }
-      }`
 
     getPosts = async (query) => {
         try {
-          const response = await axios.post('http://localhost:8080/graphql', {
-            query,
-
-          });
+          const response = await axios.get('http://gin-faceid-backend.herokuapp.com/');
         console.log(response);
-          return response.data.data.story;
+          return response.data;
       
         } catch (error) {
           // If there's an error, set the error to the state
-          console.log("graphql error")
+          console.log("getPosts error")
         }
       }
 
     async componentDidMount() {
         try {
-            const result = await this.getPosts(this.query)
-            // const result = await this.fetchInstagramPhotos('https://www.instagram.com/gin_faceid.hk/')
+            const result = await this.getPosts();
             console.log(JSON.stringify(result))
             this.setState({
-                photos: result.igPost,
+                photos: result.photos,
                 title: result.title
             })
             // Do something with the photos
         } catch (e) {
             console.error('Fetching Instagram photos failed', e)
         }
-    }
-
-    async fetchInstagramPhotos(accountUrl) {
-        const response = await axios.get(accountUrl)
-        console.log(response.data)
-        var title = response.data.match(instagramTitleRegExp)[1]
-        console.log(title)
-        var data = response.data.match(instagramRegExp)[1];
-        data = data.substring(0, data.length - 1);
-        const json = JSON.parse(data)
-        const edges = json.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges.splice(0, 8)
-        const photos = edges.map(({ node }) => {
-            return {
-                url: `https://www.instagram.com/p/${node.shortcode}/`,
-                thumbnailUrl: node.thumbnail_src,
-                displayUrl: node.display_url,
-                caption: node.edge_media_to_caption.edges[0].node.text
-            }
-        })
-        // photos.forEach(x=>console.log(x.caption.replace(/(\r\n|\n|\r)/gm, '\n')))
-        // console.log(photos);
-        return { title, photos }
     }
 
     render() {
